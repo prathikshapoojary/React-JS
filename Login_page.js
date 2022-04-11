ff00f0c2a37ddf1a48f4e8f86ec95e9a6976e5ff
Prathiksha Poojary <47886532+prathikshapoojary@users.noreply.github.com>
2022-04-11 15:41:20 +0530

    Create Login_page.js

---
+++ Login_page.js
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+  <title>login page</title>
+<style>
+.login_box{
+  text-align: center;
+  margin-left: 200px;
+  margin-right: 200px;
+  margin-top: 10em;
+  padding: 2em 2em 2em 2em;
+  border-color: whitesmoke;
+  border-width: 0.6px;
+  border-style: solid;
+  width:max-content;
+  box-shadow:0px 0.1px  0px 0.1px;
+}
+.labels{
+  text-align: left;
+  color: gray;
+  font-size:x-small;
+}
+.check_box{
+  text-align: left;
+  padding: 0%;
+  margin: 0%;
+  position: relative;
+  vertical-align: left;
+}
+</style>
+</head>
+<body>
+
+  <div class="login_box">
+    <p>Login here</p>
+    <br>
+    <p class="labels">Email Address*</p>
+    <input type="text">
+    <br><br>
+    <p class="labels">Email Address*</p>
+    <input type="text">
+    <br>
+    <br>
+    <button>Login</button>
+  </div>
+
+</body>
+</html>
